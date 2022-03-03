6b8da85d05223a7dc42d03ab3bb53f94a8e1a513
Vinicius Parente <vd.parente@hotmail.com>
2022-03-02 21:55:47 -0300

    MongoDB

---
+++ AwesomeShop.Services.Payments/Infrastructure/Repositories/MongoDbOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace AwesomeShop.Services.Payments.Api.Infrastructure.Repositories
+{
+    public class MongoDbOptions
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+    }
+}
--- AwesomeShop.Services.Payments/Startup.cs
+++ AwesomeShop.Services.Payments/Startup.cs
@@ -1,3 +1,4 @@
+using AwesomeShop.Services.Payments.Api;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,12 @@ namespace AwesomeShop.Services.Payments
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddMongo();
+
+
+
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
